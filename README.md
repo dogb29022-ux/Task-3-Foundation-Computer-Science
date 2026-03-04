0743fc39f98a274224fc5835f1a62b0d498e98da
dogb29022-ux <dogb29022@gmail.com>
2026-03-04 17:33:46 +0545

    upadated readme and sql file for inserting rawdata values

--- README.md
+++ README.md
@@ -8,6 +8,12 @@ CREATE DATABASE IF NOT EXISTS club_db;
 
 <img src="/diagrams/CREATE.png" height="300" width="300">
 
+**Feeding RawData**
 
+<img src="/diagrams/feeding rawdata.png" height="300" width="300">
+
+**Inserting RawData Value**
+
+<img src="/diagrams/rawdata value.png" height="300" width="300">
 
 ### Second Normal Form
--- queries/Mysql.sql
+++ queries/Mysql.sql
@@ -10,3 +10,15 @@ CREATE TABLE RawData (
     ClubMentor VARCHAR(100),
     JoinDate VARCHAR(20)
 );
+
+INSERT INTO RawData VALUES
+(1, 'Asha', 'asha@email.com', 'Music Club', 'R101', 'Mr. Raman', '1/10/2024'),
+(2, 'Bikash', 'bikash@email.com', 'Sports Club', 'R202', 'Ms. Sita', '1/12/2024'),
+(1, 'Asha', 'asha@email.com', 'Sports Club', 'R202', 'Ms. Sita', '1/15/2024'),
+(3, 'Nisha', 'nisha@email.com', 'Music Club', 'R101', 'Mr. Raman', '1/20/2024'),
+(4, 'Rohan', 'rohan@email.com', 'Drama Club', 'R303', 'Mr. Kiran', '1/18/2024'),
+(5, 'Suman', 'suman@email.com', 'Music Club', 'R101', 'Mr. Raman', '1/22/2024'),
+(2, 'Bikash', 'bikash@email.com', 'Drama Club', 'R303', 'Mr. Kiran', '1/25/2024'),
+(6, 'Pooja', 'pooja@email.com', 'Sports Club', 'R202', 'Ms. Sita', '1/27/2024'),
+(3, 'Nisha', 'nisha@email.com', 'Coding Club', 'Lab1', 'Mr. Anil', '1/28/2024'),
+(7, 'Aman', 'aman@email.com', 'Coding Club', 'Lab1', 'Mr. Anil', '1/30/2024');
