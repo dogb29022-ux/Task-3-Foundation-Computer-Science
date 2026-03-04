84f8a4d0998296a3542801a788a5e141125f4b73
dogb29022-ux <dogb29022@gmail.com>
2026-03-04 17:24:02 +0545

    upadated mysql file to feed rawdata

--- queries/Mysql.sql
+++ queries/Mysql.sql
@@ -1,1 +1,12 @@
 CREATE DATABASE IF NOT EXISTS club_db;
+USE club_db;
+
+CREATE TABLE RawData (
+    StudentID INT,
+    StudentName VARCHAR(100),
+    Email VARCHAR(100),
+    ClubName VARCHAR(100),
+    ClubRoom VARCHAR(50),
+    ClubMentor VARCHAR(100),
+    JoinDate VARCHAR(20)
+);
